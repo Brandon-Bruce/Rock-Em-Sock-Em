350a028dff6c6c8e19661ff02d3ffbfcc16aab9f
Nicholas Byrne <nicholas.byrne@ad.aie.edu>
2016-04-11 15:00:15 +1000

    Hi

---
+++ New Unity Project/Assets/Script/RobotMovementScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotMovementScript : MonoBehaviour {
+
+    public float m_maxSpeed = 10f;
+    public bool m_facingRight = true;
+
+    public Rigidbody m_rigidBody;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void FixedUpdate()
+    {
+        m_rigidBody.velocity = Vector3.zero;
+
+        float move = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_rigidBody.velocity = new Vector3(m_maxSpeed, m_rigidBody.velocity.y, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_rigidBody.velocity = new Vector3(-m_maxSpeed, m_rigidBody.velocity.y, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            //GetComponentInChildren
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
